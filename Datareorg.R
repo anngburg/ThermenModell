2d7d4222e5df82d6c351a7259be2e999884c6d1c
anngburg <a.grotenburg@web.de>
2025-05-04 19:29:32 +0200

    wetter added + model

--- Datareorg.R
+++ Datareorg.R
@@ -70,4 +70,43 @@ ThermenAuslastung_data <- ThermenAuslastung_data %>%
   unite(col = "Therme_20:21", 'Therme_20:00:00', 'Therme_21:00:00', sep = "", remove = TRUE, na.rm = TRUE) %>% 
   unite(col = "Therme_22:23", 'Therme_22:00:00', 'Therme_23:00:00', sep = "", remove = TRUE, na.rm = TRUE) %>%
   mutate(across(starts_with("Therme_"), as.numeric))
-    
+
+#Wetterdaten hinzufügen    
+niederschlag <- read_csv("cnwppd9c.csv")
+temperatur <- read_csv("7h617c8y.csv")
+sonnenstunden <- read_csv("lgnx5i6x.csv")
+
+wetter <- cbind(temperatur, niederschlag$'Niederschlag (6 bis 6 UTC)', sonnenstunden$Sonnenschein)
+
+wetter <- rename(wetter,
+                 Date = category,
+                 Temp_high = Höchstwert,
+                 Temp_mid = Mittelwert,
+                 Temp_low = Tiefstwert,
+                 Rain = 'niederschlag$\"Niederschlag (6 bis 6 UTC)\"',
+                 Sun = 'sonnenstunden$Sonnenschein')
+
+wetter <- wetter %>% mutate(Date = dmy(paste0(Date, "2025")))
+
+Thermen_data <- ThermenAuslastung_data %>% left_join(wetter, by = "Date")
+
+Thermen_data <- Thermen_data %>%
+                  rowwise() %>%
+                  mutate(
+                    Parkhaus_max = max(c_across(starts_with("Parkhaus_")), na.rm = TRUE),
+                    Sauna_max = max(c_across(starts_with("Sauna_")), na.rm = TRUE),
+                    Therme_max = max(c_across(starts_with("Therme_")), na.rm = TRUE)) 
+                  
+
+Thermen_data$prevSauna_max <- dplyr::lag(Thermen_data$Sauna_max, n = 1)
+Thermen_data$prevParkh_max <- dplyr::lag(Thermen_data$Parkhaus_max, n = 1)
+Thermen_data$prevTherme_max <- dplyr::lag(Thermen_data$Therme_max, n = 1)
+
+Thermen_data$Weekday <- weekdays(Thermen_data$Date)
+
+#Modell bauen
+Thermen_Modell = lm(formula = Sauna_max ~ prevSauna_max + prevSauna_max + prevTherme_max + `Parkhaus_00:01` + `Parkhaus_02:03` + `Parkhaus_04:05` + `Parkhaus_06:07` + `Parkhaus_08:09` +
+                      `Parkhaus_10:11` + `Parkhaus_12:13` +`Sauna_08:09` + `Sauna_10:11` + `Sauna_12:13` + `Therme_08:09` + `Therme_10:11` + `Therme_12:13` +
+                      Temp_high + Temp_mid + Temp_low + Sun + Rain + as.factor(Weekday), data = Thermen_data, na.action = na.exclude)
+
+summary(Thermen_Modell)
